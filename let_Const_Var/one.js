7a0b3b46d285ec1de71bed737be8ecc949d020d5
Samruddhi Chavan <103636116+Sam202003@users.noreply.github.com>
2024-05-13 04:31:51 +0000

    let var const

---
+++ let_Const_Var/one.js
@@ -0,0 +1,13 @@
+const accountId = 14444;
+let accountEmail ="sam@gmail.com";
+var accountPassword = "12345";
+accountCity ="Jaipur"
+
+// accountId = 2; //not allowed 
+accountEmail ="sam@gmail05.com";
+accountPassword = "12345678";
+accountCity ="Rajapur"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
